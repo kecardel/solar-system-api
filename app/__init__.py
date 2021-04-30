cbbbc32664a95eccc2af1e50950f534aa13c7e9c
Kaylyn Cardella <kaylyncardella@gmail.com>
2021-04-30 10:31:46 -0700

    set up and added planet model

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
+from flask_migrate import Migrate 
 
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    #hide a warning about a feature in SQLAlchemy that we won't be using
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    #set app.config['SQLALCHEMY_DATABASE_URI'] to the connection string for our database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from .routes import planets_bp
+    app.register_blueprint(planets_bp)
+    
+    from app.models.planet import Planet
+    
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,7 @@
 from flask import Blueprint
 
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+# @planets_bp.route("/planets", methods=["GET"])
+# def 
+
